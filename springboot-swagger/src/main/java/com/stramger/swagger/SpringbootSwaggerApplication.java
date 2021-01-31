7a39c64c6e6e6be536810f7af3cb831941322290
stranger <3286978744@qq.com>
2021-01-31 19:20:47 +0800

    update swagger

--- springboot-swagger/src/main/java/com/stramger/swagger/SpringbootSwaggerApplication.java
+++ springboot-swagger/src/main/java/com/stramger/swagger/SpringbootSwaggerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootSwaggerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringbootSwaggerApplication.class,args);
     }
