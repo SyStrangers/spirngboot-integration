2d8b1b58641b37297525e7ad8738c861eaf9406e
bishiyuan <3286978744@qq.com>
2021-03-02 14:48:10 +0800

    update swagger

--- springboot-swagger/src/main/java/com/stranger/swagger/SpringbootSwaggerApplication.java
+++ springboot-swagger/src/main/java/com/stranger/swagger/SpringbootSwaggerApplication.java
@@ -1,4 +1,4 @@
-package com.stramger.swagger;
+package com.stranger.swagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- springboot-swagger/src/main/java/com/stranger/swagger/config/SwaggerConfig.java
+++ springboot-swagger/src/main/java/com/stranger/swagger/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.stramger.swagger.config;
+package com.stranger.swagger.config;
 
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
